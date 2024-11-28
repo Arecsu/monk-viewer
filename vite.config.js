1285224dc7d50919be77cac09815d2ce1f1acd6d
Arecsu <alejandro9r@gmail.com>
2024-11-28 14:54:43 -0300

    Hello

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+// Common chunks configuration
+const manualChunks = {
+  manualChunks(id) {
+    if (id.includes("shared-orbitcontrols")) {
+      return "shared-orbitcontrols";
+    }
+    if (id.includes("node_modules/three")) {
+      return "vendor-three";
+    }
+  }
+};
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        'offscreencanvas-worker-orbitcontrol': path.resolve(__dirname, "src/offscreencanvas-worker-orbitcontrol.js"),
+      },
+      output: manualChunks
+    },
+  },
+  worker: {
+    format: "es",
+    plugins: [],
+    rollupOptions: {
+      output: manualChunks
+    },
+  },
+});
